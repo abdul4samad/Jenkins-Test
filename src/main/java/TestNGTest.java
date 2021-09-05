c48df0cb8ebc47c1c7071523edd0038decad6a87
Abdul Samad <abdul4samad@gmail.com>
2021-09-06 00:26:03 +0530

    stash restored

--- src/main/java/TestNGTest.java
+++ src/main/java/TestNGTest.java
@@ -4,7 +4,7 @@ public class TestNGTest {
 
     @Test
     public static void  method1(){
-        System.out.println("Test1 modified. Appended 3333");
+        System.out.println("Test1 modified. Appended 3");
     }
 
     @Test
